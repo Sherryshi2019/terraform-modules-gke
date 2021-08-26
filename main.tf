7db81addf264e10e9d48d9a0b800cc815df99053
Sherryshi2019 <54589085+Sherryshi2019@users.noreply.github.com>
2021-08-26 13:38:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
  module "gke1" {
- source  = "tfe.shi.3607853379.workshops.arctiq.ca/arctiq/gke/google"
+ source  = "tfe.shi-3607853379.workshops.arctiq.ca/arctiq/gke/google"
  version = "1.0.0"
 
  # insert required variables here
