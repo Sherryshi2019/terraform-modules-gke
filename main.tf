02bb50c7df2388cff8275ebe827d9da4fc996459
Sherryshi2019 <54589085+Sherryshi2019@users.noreply.github.com>
2021-08-26 16:07:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
- module "gke" {
- source  = "tfe.sshi.3607853379.workshops.arctiq.ca/arctiq/gke/google"
- version = "1.0.0"
+module "gke" {
+ # source  = "tfe.shayes.3607853379.workshops.arctiq.ca/arctiq/gke/google"
+   source  = "git::https://github.com/szhayes/terraform-google-gke.git?ref=1.0.0"
+ # version = "1.0.0"
 
  # insert required variables here
  cluster_name = var.cluster_name
- project_id=var.gcp_project_id
-
- }
+ project_id   
